3284b7442c0eaf558247ffe5b19a81ab27c1766e
Federico <cella.fed@gmail.com>
2021-11-08 16:23:34 +0100

    Added project page

---
+++ pages/project/[id].js
@@ -0,0 +1,14 @@
+import Viewer from '@/components/Viewer';
+
+export default function ProjectPage() {
+    const garzaImages = ['/Garza/F31 garza-360-01.jpg', '/Garza/F31 garza-360-02.jpg', '/Garza/F31 garza-360-03.jpg',
+        '/Garza/F31 garza-360-04.jpg', '/Garza/F31 garza-360-05.jpg', '/Garza/F31 garza-360-06.jpg',
+        '/Garza/F31 garza-360-07.jpg', '/Garza/F31 garza-360-08.jpg', '/Garza/F31 garza-360-09.jpg',
+        '/Garza/F31 garza-360-10.jpg', '/Garza/F31 garza-360-11.jpg', '/Garza/F31 garza-360-12.jpg',]
+
+    return (
+        <div>
+            <Viewer images={garzaImages}></Viewer>
+        </div>
+    )
+}
