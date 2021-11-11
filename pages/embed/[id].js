b6828ab20cdada78d3b9f7d9781851b58e7d7ebb
Lorenzo <lorenz.cella@gmail.com>
2021-11-11 20:24:11 +0100

    remove required user

--- pages/embed/[id].js
+++ pages/embed/[id].js
@@ -3,23 +3,8 @@ import { useRouter } from 'next/router';
 import { useUser } from '@/utils/useUser';
 import { useState, useEffect } from 'react';
 import { supabase } from '@/utils/supabase-client'
-import Image from 'next/image'
 
 export default function ProjectPage() {
-    const garzaImages = ['/Garza/F31 garza-360-01.jpg', '/Garza/F31 garza-360-02.jpg', '/Garza/F31 garza-360-03.jpg',
-        '/Garza/F31 garza-360-04.jpg', '/Garza/F31 garza-360-05.jpg', '/Garza/F31 garza-360-06.jpg',
-        '/Garza/F31 garza-360-07.jpg', '/Garza/F31 garza-360-08.jpg', '/Garza/F31 garza-360-09.jpg',
-        '/Garza/F31 garza-360-10.jpg', '/Garza/F31 garza-360-11.jpg', '/Garza/F31 garza-360-12.jpg',
-        '/Garza/F31 garza-360-13.jpg', '/Garza/F31 garza-360-14.jpg', '/Garza/F31 garza-360-15.jpg',
-        '/Garza/F31 garza-360-16.jpg', '/Garza/F31 garza-360-17.jpg', '/Garza/F31 garza-360-18.jpg',
-        '/Garza/F31 garza-360-19.jpg', '/Garza/F31 garza-360-20.jpg', '/Garza/F31 garza-360-21.jpg',
-        '/Garza/F31 garza-360-22.jpg', '/Garza/F31 garza-360-23.jpg', '/Garza/F31 garza-360-24.jpg',
-        '/Garza/F31 garza-360-25.jpg', '/Garza/F31 garza-360-26.jpg', '/Garza/F31 garza-360-27.jpg',
-        '/Garza/F31 garza-360-28.jpg', '/Garza/F31 garza-360-29.jpg', '/Garza/F31 garza-360-30.jpg',
-        '/Garza/F31 garza-360-31.jpg', '/Garza/F31 garza-360-32.jpg'
-    ]
-
-    const { userLoaded, user, session, userDetails } = useUser();
     let [project, setProject] = useState(null);
     const router = useRouter()
     const { id } = router.query
@@ -29,7 +14,7 @@ export default function ProjectPage() {
     }, [id])
 
     async function getProject() {
-        const { data, error, status } = await supabase.from('projects').select('id, user_id, name, pictures').eq('id', id).single()
+        const { data, error, status } = await supabase.from('projects').select('id, name, pictures').eq('id', id).single()
         if (error) throw error
         console.log(data)
         setProject(data)
@@ -70,7 +55,7 @@ export default function ProjectPage() {
 
     return (
         <div>
-            {paths && paths.length ? <Viewer images={paths}></Viewer> : 'Product not found'}
+            {paths && paths.length ? <Viewer images={paths} embed={true}></Viewer> : 'Product not found'}
         </div >
     )
 }
