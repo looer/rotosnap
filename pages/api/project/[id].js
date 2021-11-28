80465dc163276419ed7798f2036bd6e70d7b082c
Lorenzo <lorenz.cella@gmail.com>
2021-11-29 00:01:41 +0100

    reduced number of api calls

--- pages/api/project/[id].js
+++ pages/api/project/[id].js
@@ -4,10 +4,11 @@ export default async function handler(req, res) {
 
     const { id } = req.query
     const { data, error, status } = await supabase.from('projects').select('id, name, pictures').eq('id', id).single()
-    const paths = await getPublicUrls(data)
+    const baseURL = await getPublicUrl(data)
+
     const project = {
         ...data,
-        pictures: paths,
+        baseURL: baseURL
     }
     if (error) {
         res.status(300).json(error)
@@ -19,7 +20,6 @@ export default async function handler(req, res) {
 async function getPublicUrls(project) {
     if (project) {
         let paths = [];
-        //possible improvement: all public URL have the same base url
         for (const picture of project.pictures) {
             let newPicture = await downloadImage(picture);
             paths.push(newPicture);
@@ -27,6 +27,13 @@ async function getPublicUrls(project) {
         return paths
     }
 }
+async function getPublicUrl(project) {
+    let firstURL = await downloadImage(project.pictures[0]);
+    const baseURL = firstURL.substring(0, firstURL.lastIndexOf("/") + 1);
+    return baseURL
+}
+
+
 
 async function downloadImage(path) {
     try {
