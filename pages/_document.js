2417344a1616662c07824f686c411a6968013b29
Lorenzo <lorenz.cella@gmail.com>
2022-02-18 10:54:52 +0100

    added chat

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,13 @@ class MyDocument extends Document {
           `,
             }}
           />
+          <script dangerouslySetInnerHTML={{
+            __html: `window.$crisp = []; 
+            window.CRISP_WEBSITE_ID = '${process.env.NEXT_PUBLIC_CRISP}';
+            (function () { d = document; s = d.createElement("script"); 
+            s.src = "https://client.crisp.chat/l.js"; 
+            s.async = 1; d.getElementsByTagName("head")[0].appendChild(s); })();`,
+          }} />
         </Head>
         <body className="loading">
           <Main />
